f4adc8fd89a5004179abb7367e0fd19426bef729
Juan Barbosa <js.barbosa10@uniandes.edu.co>
2019-04-11 19:32:41 -0500

    working weird

--- Files/Week 12/lyapunov.py
+++ Files/Week 12/lyapunov.py
@@ -5,10 +5,9 @@ import smbh
 import numpy as np
 import matplotlib.pyplot as plt
 
-pos = 1e-3 * np.ones(3) / 3 ** 0.5
-dq = np.ones(3) * 1e-3
+pos = 1e-0 * np.ones(3) / 3 ** 0.5
 
-v = 70
+v = 30
 theta = np.pi / 4
 phi = np.pi / 4
 
@@ -17,51 +16,64 @@ y = v * np.sin(theta) * np.sin(phi)
 z = v * np.cos(theta)
 
 speeds = [x, y, z]
-ls = np.linspace(1e1, 0.5e3).astype(int)
-lya_s = np.zeros(len(ls))
-lya_t = np.zeros(len(ls))
+ls = np.logspace(1, 3, 10).astype(int)
+lya_s = np.zeros((len(ls), 7))
+lya_t = np.zeros((len(ls), 7))
 
+T = 1e-5
+dq = 1e-8
 
-T = 1e-4
-dq = 1e-5
-print(ls[-1], T * ls[-1])
+with open('lyapunov_s.dat', 'w') as file_s: pass
+with open('lyapunov_t.dat', 'w') as file_t: pass
+for (i, l) in enumerate(ls):
+    print(l)
+    lya_s[i] = l, *smbh.lyapunov(pos, speeds, dq, T = T, l = l, triaxial = False)
+    print('')
+    lya_t[i] = l, *smbh.lyapunov(pos, speeds, dq, T = T, l = l, triaxial = True)
 
-a = smbh.lyapunov(pos, speeds, dq, T = T, l = ls[-1], triaxial = False)
-print(a)
-# file_s = open('lyapunov_s.dat', 'w')
-# file_t = open('lyapunov_t.dat', 'w')
+    with open('lyapunov_s.dat', 'a') as file_s:
+        txt = "%d %f %f %f %f %f %f\n" % tuple(lya_s[i])
+        # print(txt)
+        file_s.write(txt)
+    with open('lyapunov_t.dat', 'a') as file_t:
+        txt = "%d %f %f %f %f %f %f\n" % tuple(lya_t[i])
+        # print(txt)
+        file_t.write(txt)
+
+# lya_s = np.genfromtxt('lyapunov_s.dat')
+# lya_t = np.genfromtxt('lyapunov_t.dat')
+
+fig, ax = plt.subplots(figsize = (8, 4.5))
+ax.plot(lya_s[:, 0] * T, lya_s[:, 1], label = "Spherical")
+ax.plot(lya_t[:, 0] * T, lya_t[:, 1], label = "Triaxial")
+# fig, axes = plt.subplots(1, 3, figsize = (8, 4.5))
+# for i in range(3):
+#     axes[i].plot(lya_s[:, 0] * T, lya_s[:, i * 2 + 1], label = 'Spherical +')
+#     axes[i].plot(lya_t[:, 0] * T, lya_t[:, i * 2 + 1], label = 'Triaxial +')
+#     axes[i].plot(lya_s[:, 0] * T, lya_s[:, i * 2 + 2], '--o', label = 'Spherical -')
+#     axes[i].plot(lya_t[:, 0] * T, lya_t[:, i * 2 + 2], '--o', label = 'Triaxial -')
 #
-# for (i, l) in enumerate(ls):
-#     print('(%d) l = %d' % (i, l))
-#     lya_s[i] = smbh.lyapunov(pos, speeds, dq, [0, 0, 0], 1, T = T, l = l, triaxial = False)
-#     lya_t[i] = smbh.lyapunov(pos, speeds, dq, [0, 0, 0], 1, T = T, l = l, triaxial = True)
-#
-#     file_s.write("%d %f\n" % (l, lya_s[i]))
-#     file_t.write("%d %f\n" % (l, lya_t[i]))
-#
-# file_s.close()
-# file_t.close()
-#
-# fig, ax = plt.subplots(figsize = (5, 4.5))
-# ax.plot(ls * T, lya_s, label = 'Spherical')
-# ax.plot(ls * T, lya_t, label = 'Triaxial')
-#
-# # ax.set_xscale('log')
-# ax.set_xlabel('$lT$')
-# ax.set_ylabel(r'$\mathcal{L}$')
-#
+#     axes[i].set_xlabel('$lT$')
+#     # axes[i].set_yscale('log')
+#     axes[i].set_xscale('log')
+# print(lya_s[-1, 0] * T)
+
+# axes[0].set_ylabel(r'$\vec{x}$')
+
 # ax.grid()
-# ax.legend()
-#
-# fig.tight_layout()
-#
-# fig.savefig('lyapunov.png', dpi = 300)
-# # fig.savefig('lyapunov_triaxial.png', dpi = 300)
-#
-# plt.show()
+# axes[0].legend()
 
+ax.set_ylabel(r'$\mathcal{L}$')
+ax.set_xlabel('$l$ $T$ (Gyr)')
 
+ax.set_xscale('log')
+ax.legend()
+ax.grid()
 
-# for l in ls:
-#     a = smbh.lyapunov(pos, speeds, d_q0 = dq, T = 1e-5, l = l, triaxial = False)
-    # print(l, a)
+fig.tight_layout()
+fig.savefig('lyapunov.png', dpi = 300)
+
+plt.show()
+
+# results = smbh.run(speeds, pot_type = smbh.SYMMETRIC)
+# print(results.RETURN_TIME)
