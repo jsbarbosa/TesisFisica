9a0cd26758ed9f4f1111069fe8ce8696635d8d13
Juan Barbosa <js.barbosa10@uniandes.edu.co>
2019-05-14 20:51:42 -0500

    almost done

--- Files/Week 11/lyapunov.py
+++ Files/Week 11/lyapunov.py
@@ -16,46 +16,48 @@ x = v * np.sin(theta) * np.cos(phi)
 y = v * np.sin(theta) * np.sin(phi)
 z = v * np.cos(theta)
 
+T = 1e-5
+
 speeds = [x, y, z]
-ls = np.linspace(1e1, 0.5e3).astype(int)
+ls = np.linspace(1e1, 1e3).astype(int)
 lya_s = np.zeros(len(ls))
 lya_t = np.zeros(len(ls))
 
-T = 1e-5
+file_s = open('lyapunov_s.dat', 'w')
+file_t = open('lyapunov_t.dat', 'w')
 
-# file_s = open('lyapunov_s.dat', 'w')
-# file_t = open('lyapunov_t.dat', 'w')
-#
-# for (i, l) in enumerate(ls):
-#     print('(%d) l = %d' % (i, l))
-#     lya_s[i] = smbh.lyapunov(pos, speeds, dq, [0, 0, 0], 1, T = T, l = l, triaxial = False)
-#     lya_t[i] = smbh.lyapunov(pos, speeds, dq, [0, 0, 0], 1, T = T, l = l, triaxial = True)
-#
-#     file_s.write("%d %f\n" % (l, lya_s[i]))
-#     file_t.write("%d %f\n" % (l, lya_t[i]))
-#
-# file_s.close()
-# file_t.close()
-#
-# fig, ax = plt.subplots(figsize = (5, 4.5))
-# ax.plot(ls * T, lya_s, label = 'Spherical')
-# ax.plot(ls * T, lya_t, label = 'Triaxial')
-#
-# # ax.set_xscale('log')
-# ax.set_xlabel('$lT$')
-# ax.set_ylabel(r'$\mathcal{L}$')
-#
-# ax.grid()
-# ax.legend()
-#
-# fig.tight_layout()
-#
-# fig.savefig('lyapunov.png', dpi = 300)
-# # fig.savefig('lyapunov_triaxial.png', dpi = 300)
-#
-# plt.show()
+for (i, l) in enumerate(ls):
+    print('(%d) l = %d' % (i, l))
+    lya_s[i] = smbh.lyapunov(speeds, dq, T = T, l = l, triaxial = False)[0]
+    lya_t[i] = smbh.lyapunov(speeds, dq, T = T, l = l, triaxial = True)[0]
 
-dq = 1e-4
+    file_s.write("%d %f\n" % (l, lya_s[i]))
+    file_t.write("%d %f\n" % (l, lya_t[i]))
+
+file_s.close()
+file_t.close()
+
+# lya_s = np.genfromtxt("lyapunov_s.dat")[:, 1]
+# lya_t = np.genfromtxt("lyapunov_t.dat")[:, 1]
+
+fig, ax = plt.subplots(figsize = (5, 4.5))
+ax.plot(ls * T, lya_s, label = 'Spherical')
+ax.plot(ls * T, lya_t, label = 'Triaxial')
+
+ax.set_xscale('log')
+# ax.set_yscale('log')
+ax.set_xlabel('$l$ $T$ (Gyr)')
+ax.set_ylabel(r'$\mathcal{L}$')
+
+ax.grid()
+ax.legend()
+
+fig.tight_layout()
+
+fig.savefig('lyapunov_.png', dpi = 300)
+# fig.savefig('lyapunov_triaxial.png', dpi = 300)
+
+plt.show()
 
 # for l in ls:
 #     a = smbh.lyapunov(pos, speeds, d_q0 = dq, T = 1e-5, l = l, triaxial = False)
